638dac94fcb1d3b9c9205dce13654357b2446909
budibrass <immortalbudi@yahoo.com>
2020-10-19 11:45:36 +0700

    done for restaurant and hospital has a Unique QR Code

--- routers/index.js
+++ routers/index.js
@@ -3,10 +3,12 @@ const userRouter = require('./userRouter')
 const hospitalRouter = require('./hospitalRouter')
 const restaurantRouter = require('./restaurantRouter')
 const historyRouter = require('./historyRouter')
+const qrRouter = require('./qrRouter')
 
 router.use('/', userRouter)
 router.use('/hospitals', hospitalRouter)
 router.use('/restaurants', restaurantRouter)
 router.use('/history', historyRouter)
+router.use('/qr', qrRouter)
 
 module.exports = router
